399df94aef8fbdb8a40533cad4a2ab220dcce055
IvanOrsh <ksironov@mail.ru>
2023-10-15 22:48:19 +0300

    feat: users migration

--- migrations/20231015191915_first_migration.ts
+++ migrations/20231015191915_first_migration.ts
@@ -1,10 +1,18 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  // create users table
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('username').notNullable().unique();
+    table.text('password').notNullable();
+    table.timestamps(true, true); // replaces two lines below!
+    // table.dateTime('created_at').defaultTo(knex.fn.now());
+    // table.dateTime('updated_at').defaultTo(knex.fn.now());
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  // drop table
+  await knex.schema.dropTable('users');
 }
-
