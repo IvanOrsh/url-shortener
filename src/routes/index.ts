e742beae347be27ffb8c7f49e6a73a78c7729f04
IvanOrsh <ksironov@mail.ru>
2023-10-16 15:01:43 +0300

    feat: set up routes - wip

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import Router from '@koa/router';
+
+import authRouter from './auth';
+
+const router = new Router();
+
+router.use('/auth', authRouter.routes(), authRouter.allowedMethods());
+
+router.use('/urls');
+
+router.use('/visits');
+
+export default router;
